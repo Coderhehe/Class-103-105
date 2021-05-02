4d7db45dce91c95ad5677bcb29b2f2f196a91ac0
Coderhehe <77240747+Coderhehe@users.noreply.github.com>
2021-05-02 10:51:41 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,38 @@
+Webcam.set({
+width:350,
+height:350,
+image_format:'png',
+png_quality:90
+});
+
+camera=document.getElementById("camera");
+Webcam.attach("#camera");
+function takep() {
+    Webcam.snap(function (data_uri){
+document.getElementById("result").innerHTML="<img id='result2' src= "+data_uri+ ">";   
+});
+}
+console.log("ml5 version",ml5.version);
+
+classifier=ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/Vk7kjHyYK/model.json",modelLoaded);
+function modelLoaded() {
+    console.log("Model Loaded!");
+}
+
+function rec() {
+    img=document.getElementById("result2");
+    classifier.classify(img,getResult);
+}
+
+function getResult(error,result) {
+    if (error) {
+        console.error(error);
+    }
+    else
+    {
+        console.log(result);
+document.getElementById("object").innerHTML=result[0].label;
+document.getElementById("accuracy").innerHTML=result[0].confidence.toFixed(3);
+    }
+}
+
